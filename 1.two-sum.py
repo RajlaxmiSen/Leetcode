12f1eb88d445e282401b1b75adfcfb265738e9e1
Rajlaxmi Sen <rajlaxmisen574@gmail.com>
2024-02-07 05:29:49 +0530

    Update 1.two-sum.py
    brute force approach

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -1,18 +1,18 @@
-#
-# @lc app=leetcode id=1 lang=python3
-#
-# [1] Two Sum
-#
-
-# @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        def twoSum(self, nums: List[int], target: int) -> List[int]:
-            value_index_map = {}
-            for i, n in enumerate(nums):
-                diff = target - n
-                if diff in value_index_map:
-                    return [value_index_map[diff] , i]
-                value_index_map[n] = i
-# @lc code=end
+        #optimal apporach
+        value_index_map = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in value_index_map:
+                return [value_index_map[diff] , i]
+            value_index_map[n] = i
+        #brute force
+        # for i, _ in enumerate(nums):
+        #     for j , _ in enumerate(nums, start=1):
+        #         if i == j or j >= len(nums) : continue
+        #         print( nums[i] , nums[j])
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j ]
+
 
